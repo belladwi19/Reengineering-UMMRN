aa17d484254812baa9f08c3a23e8a2268c9c1d25
dindaarina <79825517+dindaarina@users.noreply.github.com>
2022-12-09 14:48:22 +0700

    index + profile update

--- page/profile.php
+++ page/profile.php
@@ -1,5 +1,5 @@
-    <!-- Description -->
-    <div class="cards-1">
+<!-- Description -->
+<div class="cards-1">
     <div class="container">
         <div class="row">
 
@@ -7,7 +7,54 @@
                 <h2>Profile</h2>
                 <hr />
             </div>
-        </div> <!-- end of row -->
-    </div> <!-- end of container -->
-</div> <!-- end of cards-1 -->
-<!-- end of description -->
+            <div class='col-lg-3'>
+                            <div class='text-container'>
+                            <div class='image-wrapper' style='margin-bottom:10px;'>
+                    <img class='img-fluid' style='height:300px; width:250px;' src='images/ilyas.jpg' alt='alternative'>
+                </div> 
+            </div>
+            </div>
+            <div class='col-lg-9'>
+            <?php
+                include"koneksi.php";
+                $sql="select * from students where id_student=$_GET[id]";
+                $query=mysqli_query($koneksi,$sql); 
+                while($row=mysqli_fetch_array($query,MYSQLI_NUM)){
+                    echo"
+                    <div class='form-group'>
+                        <h4>Nama<h4>
+                        <h2>$row[1]</h2>
+                        </div>
+                        <div class='form-group'>
+                        <h4>NIM<h4>
+                        <h2>$row[2]</h2>
+                        </div>
+                        <div class='form-group'>
+                        <h4>PHONE<h4>
+                        <h2>$row[3]</h2>
+                        </div>
+                        <div class='form-group'>     
+                        <h4>Email<h4>
+                        <h2>$row[4]</h2>
+                        </div>
+                        <div class='form-group'>  
+                        <h4>Username<h4>
+                      
+                        <h2>$row[5]</h2>
+                        </div>
+                ";}
+                ?>
+        </div >
+    </div>
+</div>
+
+<script>
+function myFunction() {
+  document.getElementById("myBtn").disabled = false;
+  document.getElementById("field1").disabled = false;
+  document.getElementById("field2").disabled = false;
+  document.getElementById("field3").disabled = false;
+  document.getElementById("field4").disabled = false;
+  document.getElementById("field5").disabled = false;
+}
+</script>
